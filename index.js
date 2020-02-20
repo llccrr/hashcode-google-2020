4cfd29699eb42205d053c945be0262c9445a020d
llccrr <loic1.renaud@hotmail.fr>
2020-02-20 19:23:17 +0100

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('bonjour')
+
+let inputFileName;
+switch (process.argv[2]) {
+    case 'a':
+        inputFileName = 'a_example';
+        break;
+    case 'b':
+        inputFileName = 'b_read_on';
+        break;
+    case 'c':
+        inputFileName = 'c_incunabula';
+        break;
+    case 'd':
+        inputFileName = 'd_tough_choices';
+        break;
+    case 'e':
+        inputFileName = 'e_so_many_books';
+        break;
+    default:
+        inputFileName = 'f_libraries_of_the_world';
+}
+console.log(inputFileName)
+// solver.start(inputFileName).then(() => engine.calculatePoints(inputFileName)).then(points => console.log(points));
