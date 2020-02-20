34f47355d4da3f30b7d86e3a5097c0fbdd6d66aa
llccrr <loic1.renaud@hotmail.fr>
2020-02-20 20:21:33 +0100

    parseOutput

--- parseOutput.js
+++ parseOutput.js
@@ -6,15 +6,11 @@ const writeToFile = (path, value) =>
 
 const parseOutput = (inputFileName, libraries) => {
     let content = '';
-
     const addLine = (str) => content += `${str}\n`;
     addLine(libraries.length);
 
     libraries.forEach(library => {
-        console.log('content', content);
         addLine(library.libId + ' ' + library.books.length);
-        console.log('content', content);
-
         addLine(library.books.join(' '));
     });
     writeToFile(`./output/${inputFileName}.out`,content)
